312b50b73e40b55dea9f45d2c4e36f6646b1a678
solechko <solechko@yandex-team.ru>
2021-02-17 02:27:53 +0300

    Localization fix

---
+++ precache-manifest.547181ea1bbb3a06dfafd5d09a8f175a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fc88019460c7b8ce5b048cdb86d1817",
+    "url": "/juuust-handoff/index.html"
+  },
+  {
+    "revision": "7c5162c83ef2f990e722",
+    "url": "/juuust-handoff/static/css/2.057380c5.chunk.css"
+  },
+  {
+    "revision": "3bb99efeefe189da6d0f",
+    "url": "/juuust-handoff/static/css/main.ba45722c.chunk.css"
+  },
+  {
+    "revision": "7c5162c83ef2f990e722",
+    "url": "/juuust-handoff/static/js/2.875835d7.chunk.js"
+  },
+  {
+    "revision": "3bb99efeefe189da6d0f",
+    "url": "/juuust-handoff/static/js/main.af4e1435.chunk.js"
+  },
+  {
+    "revision": "5285137fb60d81082d89",
+    "url": "/juuust-handoff/static/js/runtime-main.20498f9e.js"
+  },
+  {
+    "revision": "49debdc33add8e433c8f85d2801a609f",
+    "url": "/juuust-handoff/static/media/multi-styles.49debdc3.gif"
+  },
+  {
+    "revision": "a31c4705a5b7cbf3996c7b1b1a4ceec8",
+    "url": "/juuust-handoff/static/media/qrcode.a31c4705.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/juuust-handoff/precache-manifest.547181ea1bbb3a06dfafd5d09a8f175a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/juuust-handoff/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
